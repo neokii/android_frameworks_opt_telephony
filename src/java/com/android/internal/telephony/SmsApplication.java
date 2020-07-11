f39deac7a56a2134649efc561749b0bba4933372
neokii <neokii@gmail.com>
2020-07-11 17:59:41 +0900

    Update SmsApplication.java

--- src/java/com/android/internal/telephony/SmsApplication.java
+++ src/java/com/android/internal/telephony/SmsApplication.java
@@ -922,6 +922,11 @@ public final class SmsApplication {
     public static boolean canSmsAppHandleAlwaysAsk(Context context) {
         final ComponentName defaultMmsApplication = SmsApplication.getDefaultMmsApplication(context,
                 false);
+        
+        if (defaultMmsApplication == null) {
+            return true;
+        }
+        
         return TextUtils.equals(DEFAULT_SYSTEM_MMS_PACKAGE_NAME,
                 defaultMmsApplication.getPackageName());
     }
